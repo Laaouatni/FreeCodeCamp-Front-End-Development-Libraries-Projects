14142edde18a89b008a1840f6bc168e87d8bcfb3
laaouatni <laaouatni.anas@outlook.it>
2023-02-19 23:46:46 +0100

    solved error 505 using <svelte:body> instead of accessing directly the document DOM property in sveltekit

--- Build-a-Drum-Machine/drum-svelte/src/lib/DrumPad.svelte
+++ Build-a-Drum-Machine/drum-svelte/src/lib/DrumPad.svelte
@@ -4,39 +4,30 @@
   export let audioLink;
   export let lastBtnName;
 
-  function playAudio() {
-    const allAudioElements =
-      document.querySelectorAll("audio");
-    const audioElement = document.getElementById(key);
-
-    allAudioElements.forEach((audioElement) => {
-      audioElement.pause();
-    });
-    audioElement.play();
-  }
-
   let thisButton;
+  let thisAudio;
+</script>
 
-  document.addEventListener("keydown", (event) => {
+<svelte:body
+  on:keydown={(event) => {
     if (event.key.toUpperCase() === key) {
       thisButton.focus();
-      console.clear();
-      playAudio();
+      thisAudio.play();
       lastBtnName = audioName;
       setTimeout(() => {
         thisButton.blur();
       }, 151);
     }
-  });
-</script>
+  }}
+/>
 
 <button
-  on:click={playAudio}
+  on:click={() => thisAudio.play()}
   on:click={() => (lastBtnName = audioName)}
   on:click={() => setTimeout(() => thisButton.blur(), 151)}
   bind:this={thisButton}
   class="border h-20 w-20 text-3xl rounded-xl bg-white hover:font-bold hover:scale-90 hover:shadow-inner transition focus:scale-150 active:scale-150 focus:rotate-6 active:rotate-6 focus:border-4 active:border-4 focus:border-blue-400 active:border-blue-400 focus:outline-none active:outline-none"
 >
   {key}
-  <audio class="clip" id={key} src={audioLink} />
+  <audio class="clip" id={key} src={audioLink} bind:this={thisAudio} />
 </button>
