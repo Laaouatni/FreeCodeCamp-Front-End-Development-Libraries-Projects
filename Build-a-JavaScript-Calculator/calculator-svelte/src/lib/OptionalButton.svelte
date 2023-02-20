de6010b5bd0c2ab54d13c6e8fb1667b5207305a0
laaouatni <laaouatni.anas@outlook.it>
2023-02-20 17:58:43 +0100

    finished calculator

--- Build-a-JavaScript-Calculator/calculator-svelte/src/lib/OptionalButton.svelte
+++ Build-a-JavaScript-Calculator/calculator-svelte/src/lib/OptionalButton.svelte
@@ -8,11 +8,17 @@
     const lastCharString = mathString[mathString.length - 1];
     const isPoint = lastCharString === POINT;
     const isOperator = ["+", "-", "*", "/"].includes(lastCharString);
+    const isThereAnotherPoint = mathString
+      .split(/[\+\-\*\/]/)
+      .pop()
+      .includes(POINT);
 
     if (!isPoint) {
-      // if the last character is a operator add a "0."
-      if (isOperator) mathString += "0" + POINT; 
-      else mathString += POINT;
+      if (isOperator) {
+        mathString += "0" + POINT;
+      } else if (!isThereAnotherPoint) {
+        mathString += POINT;
+      }
     }
   }
 </script>
@@ -20,7 +26,10 @@
 <button
   on:click={() => {
     if (text != "=") addOptional();
-    else alert("svelte.js framework is reactive, there isn't any need for this button")
+    else
+      alert(
+        "svelte.js framework is reactive, there isn't any need for this button",
+      );
   }}
   class="flex-1 text-4xl w-16 h-16 border-4 border-blue-400 text-blue-400 active:scale-90 active:bg-blue-50 transition"
 >
